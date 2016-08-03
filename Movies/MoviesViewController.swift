792ddaf056760bb5e72c6130c68b2f75572eb04b
Alex Basson <alexbasson@gmail.com>
2016-08-03 19:49:12 -0400

    Extract movies variable in MoviesViewController

--- Movies/MoviesViewController.swift
+++ Movies/MoviesViewController.swift
@@ -3,17 +3,24 @@ import UIKit
 class MoviesViewController: UIViewController {
   @IBOutlet weak var tableView: UITableView?
   var movieService: MovieService = RealMovieService()
+  var movies: [Movie] = []
+
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+
+    movies = movieService.movies()
+  }
 }
 
 extension MoviesViewController: UITableViewDataSource {
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return movieService.movies().count
+    return movies.count
   }
 
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
     if let titleLabel = cell.titleLabel {
-      titleLabel.text = movieService.movies()[indexPath.row].title
+      titleLabel.text = movies[indexPath.row].title
     }
     return cell
   }
